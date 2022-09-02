ba5ce4a27408862a57344302c8d73f3dcfffb432
Olga <ola.noskova@mail.ru>
2022-09-02 11:34:10 +0300

    проверка на четность

---
+++ #6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Проверка на четность
+Console.WriteLine("ВВедите положительное число");
+string input;
+input = Console.ReadLine();
+int userInput;
+userInput = int.Parse(input);
+
+if (userInput % 2 == 0)
+{
+    Console.WriteLine("Это четное число");
+}
+else
+{
+    Console.WriteLine("Это нечетное число");
+}
