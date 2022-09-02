e8cb4dfbb4aaf995b2d10cc10a9526de6a33e08a
Olga <ola.noskova@mail.ru>
2022-09-02 12:40:32 +0300

    Четные числа от 0 до N

---
+++ #8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Четные числа от 1 до N
+System.Console.WriteLine("Введите число");
+string N = Console.ReadLine();
+int n = int.Parse(N);
+
+for (int i=0; i<=n; i++)
+if (i%2==0)
+{
+System.Console.WriteLine(i);
+}
+
