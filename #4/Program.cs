6229c40aa5aff20e93eb289d1714907b2b5e3c9e
Olga <ola.noskova@mail.ru>
2022-09-02 10:48:29 +0300

    Максимум из трез рэндомных чисел

---
+++ #4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Максимум из трех чисел
+int numberA=new Random().Next(1,99);
+System.Console.WriteLine(numberA);
+int numberB=new Random().Next(1,99);
+System.Console.WriteLine(numberB);
+int numberC= new Random().Next(1,99);
+System.Console.WriteLine(numberC);
+
+
+int max=numberA;
+if (numberA>max) max=numberA;
+if (numberB>max) max=numberB;
+if (numberC>max) max=numberC;
+System.Console.WriteLine($"Максимальное число ={max}");
+
+
+
+
