cc2929782218ddcfbc478b6966d0f32e933adb95
Olga <ola.noskova@mail.ru>
2022-09-05 12:43:48 +0300

    торая цифра трехзначного числа

---
+++ #10/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите трехзначное число");
+string digital = Console.ReadLine();
+System.Console.WriteLine(digital[1]);
+
+
+//или
+
+System.Console.WriteLine("Введите трехзначное число");
+int number=int.Parse(Console.ReadLine());
+System.Console.WriteLine((number/10)%10);
