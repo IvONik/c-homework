0108e3d6a902f7f2f65b516f3e1b47c58f55f802
Olga <ola.noskova@mail.ru>
2022-09-06 11:40:00 +0300

    Выдает третью цифру

---
+++ #13/Program.cs
@@ -0,0 +1,18 @@
+﻿// выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+System.Console.WriteLine("Введите число");
+
+int num = int.Parse(Console.ReadLine());
+
+    if (num < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    if (num >= 100 && num < 1000)
+    {
+       System.Console.WriteLine(num%10);
+    } 
+    if (num>=1000 && num<10000)
+    {
+        System.Console.WriteLine((num/10)%10);
+    }
