0191f18d9671f5b4b527bb0cb0a223da52b7db57
Olga <ola.noskova@mail.ru>
2022-09-07 10:00:30 +0300

    commit

--- #13/Program.cs
+++ #13/Program.cs
@@ -8,11 +8,12 @@ int num = int.Parse(Console.ReadLine());
     {
         Console.WriteLine("Третьей цифры нет");
     }
-    if (num >= 100 && num < 1000)
+    
+    while (num > 999)
     {
-       System.Console.WriteLine(num%10);
-    } 
-    if (num>=1000 && num<10000)
-    {
-        System.Console.WriteLine((num/10)%10);
+        num=num/10;
+    
+        int thirdDigit = num % 10;
+
+        System.Console.WriteLine(thirdDigit);
     }
