76045fb50db4952bed412f83408d88396af788f4
Olga <ola.noskova@mail.ru>
2022-09-07 10:09:22 +0300

    принимает на вход цифру, обозначающую день недели

--- #15/Program.cs
+++ #15/Program.cs
@@ -1,48 +1,66 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
 
 
-
-
 Console.WriteLine("Введите число");
-int n = int.Parse(Console.ReadLine());
-if (n>0 && n<8)
+int num = int.Parse(Console.ReadLine());
+
+if (num == 6 || num == 7)
 {
-switch (n)
-{
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    System.Console.WriteLine("К сожаление, это будний день");
-    break;
-    case 6:
-    case 7:
     System.Console.WriteLine("Ура, выходные");
-    break;
 }
+else if (num > 0 && num < 6)
+{
+   System.Console.WriteLine("К сожаление, это будний день"); 
 }
 else
 {
     System.Console.WriteLine("это число не соответствует дню недели");
-}  
+}
+
+
+// или
+
+//Console.WriteLine("Введите число");
+//int n = int.Parse(Console.ReadLine());
+//if (n>0 && n<8)
+//{
+//switch (n)
+//{
+    //case 1:
+    //case 2:
+    //case 3:
+    //case 4:
+    //case 5:
+    //System.Console.WriteLine("К сожаление, это будний день");
+    //break;
+    //case 6:
+    //case 7:
+    //System.Console.WriteLine("Ура, выходные");
+    //break;
+//}
+//}
+//else
+//{
+    //System.Console.WriteLine("это число не соответствует дню недели");
+//}  
+
 
 // или 
-Console.WriteLine("Введите число");
-int n = int.Parse(Console.ReadLine());
-if (n==1)
-Console.WriteLine("сегодня понедельник");
-if (n==2)
-Console.WriteLine("сегодня вторник");
-if (n==3)
-Console.WriteLine("сегодня среда");
-if (n==4)
-Console.WriteLine("сегодня четверг");
-if (n==5)
-Console.WriteLine("сегодня пятница");
-if (n==6)
-Console.WriteLine("УРА, сегодня суббота");
-if (n==7)
-Console.WriteLine("Ура, сегодня воскресенье");
-if (n>7)
-System.Console.WriteLine("это число не соответствует дню недели");
+//Console.WriteLine("Введите число");
+//int n = int.Parse(Console.ReadLine());
+//if (n==1)
+//Console.WriteLine("сегодня понедельник");
+//if (n==2)
+//Console.WriteLine("сегодня вторник");
+//if (n==3)
+//Console.WriteLine("сегодня среда");
+//if (n==4)
+//Console.WriteLine("сегодня четверг");
+//if (n==5)
+//Console.WriteLine("сегодня пятница");
+//if (n==6)
+//Console.WriteLine("УРА, сегодня суббота");
+//if (n==7)
+//Console.WriteLine("Ура, сегодня воскресенье");
+//if (n>7)
+//System.Console.WriteLine("это число не соответствует дню недели");
