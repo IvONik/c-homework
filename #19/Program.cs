d14774aecda6af5b0317d4850a56c607e9cdb602
Olga <ola.noskova@mail.ru>
2022-09-09 22:28:13 +0300

    палиндром

--- #19/Program.cs
+++ #19/Program.cs
@@ -3,20 +3,22 @@
 
 Console.WriteLine("Введите пятизначное число");
 int num = int.Parse(Console.ReadLine());
-int ferstdig = num / 10000;
-int ceconddig = num / 1000 % 10;
+int firstDig = num / 10000;
+int secondDig = num / 1000 % 10;
 int fourthDig = num / 10 % 10;
-int fifthGig = num % 10;
+int fifthDig = num % 10;
 
 if (num>=10000 && num<=99999)
-    if (ferstdig == fifthGig)
     {
-        if (ceconddig == fourthDig)
-        System.Console.WriteLine($"Число {num} является палиндромом");
-    }
-    else
-    {
+        if (firstDig == fifthDig && secondDig == fourthDig)
+        {
+            System.Console.WriteLine($"Число {num} является палиндромом");
+        }
+    
+        else
+        {
         System.Console.WriteLine($"Число {num} НЕ является палиндромом");
+        }
     }
 else
 {
