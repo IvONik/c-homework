fb9a63614a0921f224658eebc22ff89e07181c0c
Olga <ola.noskova@mail.ru>
2022-09-09 22:19:42 +0300

    таблица кубов

---
+++ #23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("Введите число");
+int N=int.Parse(Console.ReadLine());
+
+void multipl(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        int cube = i * i * i;
+        System.Console.WriteLine($"{i} в ^ 3 = {cube}");
+    }
+}
+multipl(N);
