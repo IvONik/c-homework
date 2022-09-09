c3f5876bb82f9bd8f83d5c465ed5f2d74de1671d
Olga <ola.noskova@mail.ru>
2022-09-09 10:47:42 +0300

    Сумма квадратов

---
+++ dop1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Пользователь вводит x и y. Программа печатает, чему равно x² + y².
+
+System.Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите число");
+int num2 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"Сумма квадратов равна {Math.Pow(num1,num2)} ");
