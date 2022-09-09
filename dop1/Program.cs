29a85693b05780ab75dd0c48cc633ab48ff11815
Olga <ola.noskova@mail.ru>
2022-09-09 22:33:13 +0300

    Сумма кваратов

--- dop1/Program.cs
+++ dop1/Program.cs
@@ -5,4 +5,4 @@ int num1 = int.Parse(Console.ReadLine());
 System.Console.WriteLine("Введите число");
 int num2 = int.Parse(Console.ReadLine());
 
-System.Console.WriteLine($"Сумма квадратов равна {Math.Pow(num1,num2)} ");
+System.Console.WriteLine($"Сумма квадратов равна {Math.Pow(num1,2)+Math.Pow(num2,2)} ");
