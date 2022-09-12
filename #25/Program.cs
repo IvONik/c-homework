4135453636bebfcb95df02bb0fb273180e3b0ec3
Olga <ola.noskova@mail.ru>
2022-09-12 10:44:53 +0300

    принимает на вход два числа (A и B) и возводит число A в натуральную степень B

---
+++ #25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+System.Console.WriteLine("Введите число a");
+int a = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Введите число b");
+int b = int.Parse(Console.ReadLine()!);
+
+
+double exponentiation(int a, int b)
+{
+    double exponen = Math.Pow(a, b);
+    return exponen;
+}
+double exponentiation1 = exponentiation(a, b);
+System.Console.WriteLine($"Число {a} в степени {b} равно {exponentiation1}");
