b95a6141f528b4a718b6d6ce5e6ec96092695df8
Olga <ola.noskova@mail.ru>
2022-09-13 00:32:25 +0300

    заполняет массив 8 случайными числами. Напишите функцию, которая находит среднее арифметическое чисел в массиве

---
+++ dopA/Program.cs
@@ -0,0 +1,32 @@
+﻿// заполняет массив 8 случайными числами. Напишите функцию, которая находит среднее арифметическое чисел в массиве.
+int[] array = new int[8];
+int length = array.Length;
+rdm(length);
+average();
+//
+//System.Console.WriteLine("");
+//System.Console.WriteLine(average());
+
+
+void rdm(int length)
+{
+    
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        System.Console.Write($"{array[i]}  ");
+    }
+}
+
+void average()
+{
+double summ = 0;
+int length = array.Length;
+for (int i = 0; i < length; i++)
+    {
+        summ = summ + array[i];
+}
+
+System.Console.WriteLine("");
+System.Console.WriteLine($" Среднее арифметическое {summ/length}");
+}
