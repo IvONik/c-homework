82a35833773911b9dd66d4a095c4ee0f507ec120
Olga <ola.noskova@mail.ru>
2022-09-13 14:29:37 +0300

    заполняет массив 8 случайными числами,проверяет,что все числа в массиве чётные

---
+++ dopV/Program.cs
@@ -0,0 +1,49 @@
+﻿//  Программа заполняет массив 8 случайными числами. 
+//Напишите функцию, которая проверяет, верно ли, что все числа в массиве чётные. 
+//Наличие хотя бы одного нечётного числа означает, что ответ — нет.
+Console.WriteLine("Hello, World!");
+int[] array = new int[8];
+int length = array.Length;
+
+rdm(length);
+element(System.Console.WriteLine());
+
+void rdm(int length)
+{
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        System.Console.Write($"{array[i]}  ");
+    }
+}
+
+
+
+System.Console.WriteLine(" ");
+
+void element(int length)
+{
+double[] element = new double[array.Length];
+int count1 = 0;
+
+for (int i = 0; i < length; i++)
+{
+    element[i] = array[i] % 2;
+    System.Console.Write(element[i]);
+
+        if (element[i] == 0)
+        {
+            count1 = count1++;
+        }
+}
+System.Console.WriteLine(" ");
+    if (count1 == 8)
+    {
+        System.Console.WriteLine("Все элементы четные");
+    }
+    else
+    {
+        System.Console.WriteLine("есть нечетные элементы");
+    }
+}
