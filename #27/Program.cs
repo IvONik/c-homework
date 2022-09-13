43cbaffb0a22088c849760d7c72a69599924a2bf
Olga <ola.noskova@mail.ru>
2022-09-13 12:17:26 +0300

    Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе

---
+++ #27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+System.Console.WriteLine("Введите число ");// Ввод через пробел
+string text = Console.ReadLine()!;
+string[] textarray = text.Split(" ");
+
+int summ = 0;
+
+int[] dig = new int[textarray.Length];
+for (int i = 0; i < textarray.Length; i++)
+{
+    dig[i] = int.Parse(textarray[i]);
+    //System.Console.Write($"{dig[i]} ");
+    summ = summ + dig[i];
+}
+System.Console.WriteLine($"Сумма цифр {summ}");
+
+//ломается если после последней цифры ввести пробел
