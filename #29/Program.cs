0f93449de008c0f2f6a8a788527c42a9a16b86e9
Olga <ola.noskova@mail.ru>
2022-09-14 10:24:10 +0300

    Пользователь вводит 8 чисел. Поместите их в массив и распечатайте

---
+++ #29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит 8 чисел. Поместите их в массив и распечатайте.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+System.Console.WriteLine("Введите числа");
+string numbers = Console.ReadLine(); // Предполагается, что пользователь ввел числа через пробел
+string[] array = numbers.Split (" "); 
+int length=array.Length;
+int i=0;
+while (i<length)
+{
+    int arr=int.Parse(array[i]);
+    System.Console.Write($"{arr}, ");
+    i++;
+}
