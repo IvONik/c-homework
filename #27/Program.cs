33cdbc28f545905cf92696ba1d82ffa6952704ae
Olga <ola.noskova@mail.ru>
2022-09-20 11:46:50 +0300

    принимает на вход число и выдаёт сумму цифр в числе

--- #27/Program.cs
+++ #27/Program.cs
@@ -1,16 +1,25 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-System.Console.WriteLine("Введите число ");// Ввод через пробел
-string text = Console.ReadLine()!;
-string[] textarray = text.Split(" "); //сщздали текстовый массив
+System.Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!);
 
-int summ = 0;
-
-int[] dig = new int[textarray.Length]; //сщздали цифровой массив
-for (int i = 0; i < textarray.Length; i++)
+int summ=0;
+while(number>0)
 {
-    dig[i] = int.Parse(textarray[i]); //положили из текстового массива в цифровой
-    summ = summ + dig[i];
+summ+=number%10;
+number=number/10;
 }
 System.Console.WriteLine($"Сумма цифр {summ}");
+//string[] textarray = text.Split(" "); //сщздали текстовый массив
+//string[] textarray = new string[text.Length];
+
+
+//int[] dig = new int[text.Length]; //сщздали цифровой массив 
+//int summ = 0;
+//for (int i = 0; i < text.Length; i++)
+//{
+    //dig[i] = int.Parse(textarray.Length[i]); //положили из текстового массива в цифровой
+    //summ = summ + dig[i]; 
+//}
+//System.Console.WriteLine($"Сумма цифр {summ}");
 
 //ломается если после последней цифры ввести пробел
