21764a734c4df489f20caefd7fe2b9a52974b35b
Olga <ola.noskova@mail.ru>
2022-09-26 10:45:11 +0300

    массив MxN, заполненный случайными вещественными числами числами

---
+++ #47/Program.cs
@@ -0,0 +1,32 @@
+﻿// задать двумерный массив MxN, заполненный случайными вещественными числами числами
+System.Console.WriteLine("Введите количетсво строк");
+int rows=int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите количетсво столбцов");
+int columns=int.Parse(Console.ReadLine()!);
+
+double[,] matrix=new double[rows,columns];
+
+FillArrayMatrix(matrix);
+PrintMatrix (matrix);
+
+void FillArrayMatrix(double[,] matrix)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i,j]=Math.Round(new Random().NextDouble() + new Random().Next(-10, 11), 1);
+        }
+    }
+}
+void PrintMatrix (double[,] matrix)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            System.Console.Write($"{matrix[i,j],5} ");
+        }
+        System.Console.WriteLine(" ");
+    }
+}
