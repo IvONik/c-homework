d0556ae7ea987fcb4a0d2d9bbe9990e80647cf16
Olga <ola.noskova@mail.ru>
2022-10-03 11:04:16 +0300

    Напишите программу, //которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии

---
+++ #64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+string ShowNumbers (int n)
+{
+    if (n>=1)
+    {
+    return $"{n} "+ShowNumbers(n-1);
+    }
+    else
+    {
+    return string.Empty;
+    }
+}
+System.Console.WriteLine(ShowNumbers(5));
