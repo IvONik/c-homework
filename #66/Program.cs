261680303d2646a81141ed1aba527be86abf60bc
Olga <ola.noskova@mail.ru>
2022-10-03 11:05:04 +0300

    Напишите программу, //которая найдёт сумму натуральных элементов в промежутке от M до N.

---
+++ #66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int FindSumm (int m, int n)
+{
+    if (m>n)
+    {
+        return 0;
+    }
+    else
+    {
+        return m+FindSumm(m+1,n);
+    }
+
+}
+System.Console.WriteLine(FindSumm(2,6));
